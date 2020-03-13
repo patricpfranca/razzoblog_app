4e8cc138ce3f99bf0da168e90e37ac19969fdfeb
Patric Pereira <patric.pereirafranca@gmail.com>
2020-03-12 23:43:14 -0300

    adding date format

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { View, Text } from 'react-native';
 
 import { MaterialIcons } from '@expo/vector-icons';
+import { parseISO, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import PropTypes from 'prop-types';
 
 import { colors } from '~/styles';
@@ -9,6 +11,12 @@ import { colors } from '~/styles';
 import styles from './style';
 
 export default function List({ data }) {
+  const dateParsed = useMemo(() => {
+    return format(parseISO(data.date), "dd'/'MM'/'yyyy", {
+      locale: pt,
+    });
+  }, [data.date]);
+
   return (
     <View style={styles.container}>
       <View style={styles.boxIcon}>
@@ -16,8 +24,10 @@ export default function List({ data }) {
       </View>
       <View style={styles.info}>
         <Text style={styles.txtInfo}>{data.author}</Text>
-        <Text style={styles.title}>{data.title}</Text>
-        <Text style={styles.txtInfo}>{data.date}</Text>
+        <Text style={styles.title} numberOfLines={1}>
+          {data.title}
+        </Text>
+        <Text style={styles.txtInfo}>{dateParsed}</Text>
       </View>
     </View>
   );
